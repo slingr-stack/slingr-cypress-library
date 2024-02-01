9d42619c4af18ce10144553a237e08dd05277a93
solduplessis <mduplessis@slingr.io>
2024-02-01 16:51:55 -0300

    Update iframes command

--- src/iframes.js
+++ src/iframes.js
@@ -2,11 +2,11 @@
 // These commands are for handler Slingr platform iframes
 // *******************************************************
 
-Cypress.Commands.add('getIframeBody', () => {
+Cypress.Commands.add('getIframeBody', (selector) => {
     // get the iframe > document > body
     // and retry until the body element is not empty
     return cy
-        .get('.card iframe')
+        .get(selector)
         .its('0.contentDocument.body').should('not.be.empty')
         // wraps "body" DOM element to allow
         // chaining more Cypress commands, like ".find(...)"
