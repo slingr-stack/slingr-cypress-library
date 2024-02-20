c1a2e1ec8fd06d0696ae84c79a2160d83e0883ca
solduplessis <mduplessis@slingr.io>
2024-02-20 16:35:19 -0300

    New iframe command

--- src/iframes.js
+++ src/iframes.js
@@ -6,10 +6,40 @@ Cypress.Commands.add('getIframeBody', (selector) => {
     // get the iframe > document > body
     // and retry until the body element is not empty
     return cy
-        .get(selector)
-        .its('0.contentDocument.body').should('not.be.empty')
+        .get(selector, { timeout: 5000 })
+        .its('0.contentDocument').should('exist')
+        .its('body').should('not.be.undefined')
         // wraps "body" DOM element to allow
         // chaining more Cypress commands, like ".find(...)"
         // https://on.cypress.io/wrap
         .then(cy.wrap)
+})
+
+
+/**
+ * Returns the value of the record column sent by parameter that is inside a iFrame
+ * e.i:
+ * | Profile | Status   | Date |
+ * ---------------------------
+ * | Micro   | New      | 1/12 |
+ * ---------------------------
+ * | Water   | Verified | 2/09 |
+ * -----------------------------
+ * cy.iFrameGetTableValueByRecord("iframeSelector","Status", "Micro") => return "New"
+ @param {string} iframeSelector - iframe selector
+ @param {string} columnName - The column name of the table, exact match
+ @param {string} record - The record (row)
+ @returns {string} - return 1 value of the table
+ */
+ Cypress.Commands.add('iFrameGetTableValueByRecord', (iframeSelector, columnName, record) => {
+    return cy
+        .getIframeBody(iframeSelector).contains('th', new RegExp("^" + columnName + "$"))
+        .invoke('index')
+        .then((index) => {
+            cy.getIframeBody(iframeSelector).contains('td', record)
+                .should('have.length', 1)
+                .siblings()
+                .eq(index - 1)
+                .invoke('text')
+        })
 })
