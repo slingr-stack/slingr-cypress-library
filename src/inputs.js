0fcf94327fbc412f7c4a8025509c3369a31f07f5
solduplessis <mduplessis@slingr.io>
2024-05-09 16:30:56 -0300

    selectValue fix

--- src/inputs.js
+++ src/inputs.js
@@ -7,8 +7,8 @@
  * @param {string} value - The value to be selected
  */
 Cypress.Commands.add('selectValue', { prevSubject: 'element' }, (subject, value) => {
-    cy.wrap(subject).type(value)
-    cy.waitForNetworkIdle('GET', 1000, { log: false })
+    cy.wrap(subject).type(value, { delay: 1 })
+    cy.waitForNetworkIdle('POST, GET', 1000, { log: false })
     cy.get('[role="listbox"]').contains(new RegExp("^" + value, "g")).click();
     cy.waitForNetworkIdle('POST, GET', 2000, { log: false })
 })
