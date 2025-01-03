89ffa4e1bed65e6e09efea7cfe7559542dcfee3a
fturco <142924689+fturco@users.noreply.github.com>
2025-01-03 11:14:58 -0300

    Update inputs.js

--- src/inputs.js
+++ src/inputs.js
@@ -9,6 +9,8 @@
 Cypress.Commands.add('selectValue', { prevSubject: 'element' }, (subject, value) => {
     cy.wrap(subject).type(value, { delay: 1 })
     cy.waitForNetworkIdle('POST, GET', 1500, { log: false })
-    cy.get('[role="listbox"]').contains(new RegExp("^" + value, "g")).click('top');
+    //cy.get('[role="listbox"]').contains(new RegExp("^" + value, "g")).click('top');
+    const escapedValue = value.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    cy.get('[role="listbox"]').contains(new RegExp("^" + escapedValue + "$", "g")).click('top');
     cy.waitForNetworkIdle('POST, GET', 2000, { log: false })
 })
