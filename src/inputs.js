65be9c13682fa7081a66357ee6f953e504635f5a
solduplessis <mduplessis@slingr.io>
2025-01-08 11:45:30 -0300

    remove duplicaate command

--- src/inputs.js
+++ src/inputs.js
@@ -27,17 +27,6 @@ Cypress.Commands.add('selectValueMatchBeginning', { prevSubject: 'element' }, (s
 })
 
 /**
- * Chainable, select the option that start with the value send by parameter in a dropdown input
- * @param {string} value - The value to be selected
- */
-Cypress.Commands.add('selectValueMatchBeginning', { prevSubject: 'element' }, (subject, value) => {
-    cy.wrap(subject).type(value, { delay: 1 })
-    cy.waitForNetworkIdle('POST, GET', 1500, { log: false })
-    cy.get('[role="listbox"]').contains(new RegExp("^" + value, "g")).click('top');
-    cy.waitForNetworkIdle('POST, GET', 2000, { log: false })
-})
-
-/**
  * Chainable, select the value send by parameter in a dropdown input (partial match)
  * @param {string} value - The value to be selected
  */
